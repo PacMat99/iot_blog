63e5b8d22b38ae9ac6eea3c55e63f7d7b9820d93
PacMat99 <pacchinmattia@gmail.com>
2024-02-29 10:05:27 +0100

    gemfile.lock update

---
+++ code/esp32_tests/main/main.ino
@@ -0,0 +1,288 @@
+// sds011 code explanation
+// https://electronicsinnovation.com/interfacing-sds011-air-quality-sensor-with-esp8266-diy-air-pollution-monitor-part1/
+
+// libraries for display
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Fonts/FreeMono9pt7b.h>
+
+// libraries for SDS011
+#include <SoftwareSerial.h>
+#include <esp_sds011.h>
+
+// libraries for MQTT
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+// SDS011 global variables
+#define SDS_PIN_RX 16
+#define SDS_PIN_TX 17
+
+#ifdef ESP32
+HardwareSerial& serialSDS(Serial2);
+Sds011Async< HardwareSerial > sds011(serialSDS);
+#else
+EspSoftwareSerial::UART serialSDS;
+Sds011Async< EspSoftwareSerial::UART > sds011(serialSDS);
+#endif
+
+constexpr int pm_tablesize = 20;
+int pm2_5_table[pm_tablesize];
+int pm10_table[pm_tablesize];
+
+bool is_SDS_running = true;
+
+// SDS011 auxiliary functions
+
+void start_SDS() {
+  Serial.println("Start wakeup SDS011");
+
+  if (sds011.set_sleep(false)) {
+    is_SDS_running = true;
+  }
+
+  Serial.println("End wakeup SDS011");
+}
+
+void stop_SDS() {
+  Serial.println("Start sleep SDS011");
+
+  if (sds011.set_sleep(true)) {
+    is_SDS_running = false;
+  }
+
+  Serial.println("End sleep SDS011");
+}
+
+// SSD1306 global variables
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+// MQTT global variables
+const char* ssid = "SSID";
+const char* password = "PASSWORD";
+
+const char* mqtt_server = "IP_ADDRESS";
+
+// MQTT broker credentials
+const char* MQTT_username = "USERNAME";
+const char* MQTT_password = "MQTT_PASSWORD";
+
+// Initializes the espClient. You should change the espClient name if you have multiple ESPs running in your home automation system
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+// MQTT auxiliary functions
+
+// Don't change the function below. This functions connects your ESP8266 to your router
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("WiFi connected - ESP IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+// This functions is executed when some device publishes a message to a topic that your ESP8266 is subscribed to
+// Change the function below to add logic to your program, so when a device publishes a message to a topic that 
+// your ESP8266 is subscribed you can actually do something
+void callback(String topic, byte* message, unsigned int length) {
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
+  
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+  Serial.println();
+}
+
+// This functions reconnects your ESP8266 to your MQTT broker
+// Change the function below if you want to subscribe to more topics with your ESP8266 
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP8266Client", MQTT_username, MQTT_password)) {
+      Serial.println("connected");
+    }
+    else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  // Initialize Serial communication with the computer
+  Serial.begin(115200);
+
+  Serial.println("Initializing SDS011 Air Quality Monitor...");
+
+// Initialize Serial communication with SDS011
+#ifdef ESP32
+  serialSDS.begin(9600, SERIAL_8N1, SDS_PIN_RX, SDS_PIN_TX);
+  delay(100);
+#else
+  serialSDS.begin(9600, SWSERIAL_8N1, SDS_PIN_RX, SDS_PIN_TX, false, 192);
+#endif
+
+  delay(100); // Short delay after initializing Serial
+  Sds011::Report_mode report_mode;
+  constexpr int GETDATAREPORTINMODE_MAXRETRIES = 2;
+  for (auto retry = 1; retry <= GETDATAREPORTINMODE_MAXRETRIES; ++retry) {
+    if (!sds011.get_data_reporting_mode(report_mode)) {
+      if (retry == GETDATAREPORTINMODE_MAXRETRIES) {
+        Serial.println("Sds011::get_data_reporting_mode() failed");
+      }
+    }
+    else {
+      break;
+    }
+  }
+  if (Sds011::REPORT_ACTIVE != report_mode) {
+    Serial.println("Turning on Sds011::REPORT_ACTIVE reporting mode");
+    if (!sds011.set_data_reporting_mode(Sds011::REPORT_ACTIVE)) {
+      Serial.println("Sds011::set_data_reporting_mode(Sds011::REPORT_ACTIVE) failed");
+    }
+  }
+
+  delay(100);  // Short delay after initializing Serial
+
+  while (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println("Failed to boot SSD1306");
+    delay(500);
+  }
+  display.clearDisplay();
+  display.setFont(&FreeMono9pt7b);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 10);
+  display.print("Booting...");
+  display.display();
+
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+
+  delay(2000);
+}
+
+void loop() {
+  constexpr uint32_t down_s = 30;
+
+  stop_SDS();
+  Serial.print("stopped SDS011 (is running = ");
+  Serial.print(is_SDS_running);
+  Serial.println(')');
+
+  uint32_t deadline = millis() + down_s * 1000;
+  while (static_cast<int32_t>(deadline - millis()) > 0) {
+    delay(1000);
+    Serial.println(static_cast<int32_t>(deadline - millis()) / 1000);
+    sds011.perform_work();
+  }
+
+  constexpr uint32_t duty_s = 30;
+
+  start_SDS();
+  Serial.print("started SDS011 (is running = ");
+  Serial.print(is_SDS_running);
+  Serial.println(')');
+
+  float pm2_5;
+  float pm10;
+
+  sds011.on_query_data_auto_completed([](int n) {
+    Serial.println("Begin Handling SDS011 query data");
+    Serial.print("n = "); Serial.println(n);
+    if (sds011.filter_data(n, pm2_5_table, pm10_table, pm2_5, pm10) && !isnan(pm10) && !isnan(pm2_5)) {
+      Serial.print("PM10: ");
+      Serial.print(pm10 / 10, 2);
+      Serial.print(" µg/m³   ");
+      Serial.print("PM2.5: ");
+      Serial.print(pm2_5 / 10, 2);
+      Serial.println(" µg/m³");
+    }
+    Serial.println("End Handling SDS011 query data");
+  });
+
+  if (!sds011.query_data_auto_async(pm_tablesize, pm2_5_table, pm10_table)) {
+    Serial.println("measurement capture start failed");
+  }
+
+  deadline = millis() + duty_s * 1000;
+  while (static_cast<int32_t>(deadline - millis()) > 0) {
+    delay(1000);
+    Serial.println(static_cast<int32_t>(deadline - millis()) / 1000);
+    sds011.perform_work();
+  }
+
+  String pm2_5_str = String(pm2_5, 2);
+  String pm10_str = String(pm10, 2);
+  // Print the values
+  Serial.print("PM2.5: ");
+  Serial.print(pm2_5, 2);  // 2 decimal places
+  Serial.print(" µg/m³   ");
+  Serial.print("PM10: ");
+  Serial.print(pm10, 2);  // 2 decimal places
+  Serial.println(" µg/m³   ");
+
+  display.clearDisplay();
+  display.setFont(&FreeMono9pt7b);
+  display.setTextColor(WHITE);
+
+  display.setCursor(0, 10);
+  display.print("Analyzing...");
+  display.display();
+  delay(1000);
+
+  display.setCursor(0, 30);
+  display.print("PM2.5: ");
+  display.setCursor(0, 50);
+  display.print(pm2_5_str);  // 2 decimal places
+  display.print(" µg/m³");
+  display.display();
+  delay(3000);
+
+  display.clearDisplay();
+  display.setFont(&FreeMono9pt7b);
+  display.setTextColor(WHITE);
+
+  display.setCursor(0, 10);
+  display.print("Analyzing...");
+  display.setCursor(0, 30);
+  display.print("PM10: ");
+  display.setCursor(0, 50);
+  display.print(pm10_str);  // 2 decimal places
+  display.print(" µg/m³");
+  display.display();
+  delay(3000);
+
+  // MQTT connection and sending data to nodered
+  if (!client.connected())
+    reconnect();
+  if(!client.loop())
+    client.connect("ESP8266Client", MQTT_username, MQTT_password);
+  // Publishes pm2_5 and pm10 values    
+  client.publish("air_quality_monitor/pm2_5", pm2_5_str.c_str());
+  client.publish("air_quality_monitor/pm10", pm10_str.c_str());
+}
